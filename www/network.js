e86f331fdf1f1e802e3e73a468640947151d245e
Rob Currie <rcurrie@ucsc.edu>
2016-10-11 13:13:56 -0700

    Switch to d3 force layout for a more organic less hierarchical network diagram

--- www/network.js
+++ www/network.js
@@ -1,57 +1,102 @@
 function draw_network(stewards) {
-  var cy = cytoscape({
-    container: document.getElementById('network'),
-    style: [
-      {
-        selector: 'node',
-        style: {
-          'content': 'data(id)',
-          'text-opacity': 0.9,
-          'text-valign': 'center',
-          'text-halign': 'center',
-          'text-wrap': 'wrap',
-          'text-transform': 'lowercase',
-          'text-margin-y': '8px',
-          'background-color': '#11479e',
-          'background-opacity': 0.5,
-          label: 'data(label)'
-        }
-      },
-      {
-        selector: 'edge',
-        style: {
-          'width': 4,
-          'target-arrow-shape': 'triangle',
-          'line-color': '#9dbaea',
-          'target-arrow-color': '#9dbaea',
-          'curve-style': 'bezier'
-        }
-      }
-    ],
-  });
-
 
   var addresses = _.keys(stewards);
-  for (var i=0; i < addresses.length; i++) {
-    cy.add({data: {id: addresses[i],
-      label: stewards[addresses[i]]._source.submissions.length + "\n" +
-      stewards[addresses[i]]._source.domain}});
-  }
-
+  var links = [];
   for (var i=0; i < addresses.length; i++) {
     var peers = stewards[addresses[i]]._source.peers;
     for (var j=0; j < peers.length; j++) {
-      cy.add({data: {id: 'edge' + i + j, source: addresses[i], target: peers[j]}});
+      links.push({source: stewards[addresses[i]], 
+        target: stewards[peers[j]], value: 1.0});
     }
   }
 
-  cy.layout({
-    name: 'dagre'
-  });
+  var svg = d3.select("body").append("svg");
 
-  cy.on('tap', 'node', function(event) {
-    window.location.href = "/cgtd/index.html?steward=" + this.data("id");
-  });
+  var force = d3.layout.force()
+    .nodes(_.values(stewards))
+    .links(links)
+    .linkDistance(120)
+    .charge(-900)
+    .on("tick", tick)
+    .start();
+
+
+  // build the arrow.
+  svg.append("svg:defs").selectAll("marker")
+    .data(["end"])      // Different link/path types can be defined here
+    .enter().append("svg:marker")    // This section adds in the arrows
+    .attr("id", String)
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 10)
+    .attr("markerHeight", 10)
+    .attr("orient", "auto")
+    .append("svg:path")
+    .attr("d", "M0,-5L10,0L0,5");
+
+  // add the links and the arrows
+  var path = svg.append("svg:g").selectAll("path")
+    .data(force.links())
+    .enter().append("svg:path")
+  //    .attr("class", function(d) { return "link " + d.type; })
+    .attr("class", "link")
+    .attr("marker-end", "url(#end)");
+
+  // define the nodes
+  var node = svg.selectAll(".node")
+    .data(force.nodes())
+    .enter().append("g")
+    .attr("class", "node")
+    .call(force.drag);
+
+  // add the nodes
+  node.append("circle")
+    .attr("r", 16)
+    .on("click", function(e) {
+      if (d3.event.defaultPrevented) return; // skip if drag
+      window.location.href = "/cgtd/index.html?steward=" + e._id;
+    });
+
+  // add the text 
+  node.append("text")
+    .attr("x", 18)
+    .attr("dy", ".35em")
+    .text(function(d) { return d._source.domain.toLowerCase(); });
+  node.append("text")
+    .attr("x", 0)
+    .attr("dy", ".35em")
+    .attr("text-anchor", "middle")
+    .text(function(d) { return d._source.submissions.length; });
+
+  // add the curvy lines
+  function tick() {
+    path.attr("d", function(d) {
+      var dx = d.target.x - d.source.x,
+        dy = d.target.y - d.source.y,
+        dr = Math.sqrt(dx * dx + dy * dy);
+      return "M" + 
+        d.source.x + "," + 
+        d.source.y + "A" + 
+        dr + "," + dr + " 0 0,1 " + 
+        d.target.x + "," + 
+        d.target.y;
+    });
+
+    node.attr("transform", function(d) { 
+      return "translate(" + d.x + "," + d.y + ")"; 
+    });
+  }
+
+  resize();
+  d3.select(window).on("resize", resize);
+
+  function resize() {
+    width = window.innerWidth, height = window.innerHeight;
+    svg.attr("width", width).attr("height", height);
+    force.size([width, height]).resume();
+  }
+
 }
 
 $('document').ready(function() {
@@ -61,31 +106,31 @@ $('document').ready(function() {
 
   $("#search").submit(function(event) {
     query = $("#query").val(); 
-		var request = {
-        "query": {
-            "match": {
-                "_all": {
-                    "query": query,
-                    "operator": "and"
-                }
-            }
-        },
-        "size": 100 
+    var request = {
+      "query": {
+        "match": {
+          "_all": {
+            "query": query,
+            "operator": "and"
+          }
+        }
+      },
+      "size": 100 
     }
-		$.ajax({ 
-			url: "/es/cgt/submission/_search", 
-			type: "POST", 
-			dataType: "json", 
-			data: JSON.stringify(request), 
-			success: function(results) {
-				console.log(results);
-				$("#network").hide();
-				$("#results").html(resultsTemplate({results: results}));
-			}
-		}); 
+    $.ajax({ 
+      url: "/es/cgt/submission/_search", 
+      type: "POST", 
+      dataType: "json", 
+      data: JSON.stringify(request), 
+      success: function(results) {
+        console.log(results);
+        $("svg").hide();
+        $("#results").html(resultsTemplate({results: results}));
+      }
+    }); 
     event.preventDefault();
     ga('send', 'event', 'submissions', 'search', query);
-	});
+  });
 
   $.ajax({ 
     url: "/es/cgt/steward/_search/?size=1000", 
