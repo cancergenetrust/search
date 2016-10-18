1d58e007d8da9e4d7f483fc7d8787095cd206f44
Rob Currie <rcurrie@ucsc.edu>
2016-10-18 11:01:36 -0700

    Commit to conform to actual demo

--- demo/reset.sh
+++ demo/reset.sh
@@ -3,8 +3,8 @@ address=`docker exec -it demo_cgtd curl localhost:5000/v0/address | jq -r '.addr
 echo "Deleting peer from this server to demo steward at $address"
 docker exec -it cgtd curl -X DELETE localhost:5000/v0/peers/$address
 
-echo "Deleting peer from demo steward to nki"
-docker exec -it demo_cgtd curl -X DELETE localhost:5000/v0/peers/Qmanw7MzPbEkd3FSkeH8NHi3d8k1nHP1mU3S8LU4NCEg7o
-
 echo "Deleting demo steward $address from elastic search"
 docker exec -it es curl -X DELETE localhost:9200/cgt/steward/$address
+
+echo "Removing all submissions"
+docker exec demo_ipfs sh -c "echo '{\"domain\": \"kaiserpermanente.org\", \"submissions\": [], \"peers\": []}' | ipfs add -q | xargs ipfs name publish"
