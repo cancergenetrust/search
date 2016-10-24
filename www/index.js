372df58a3f1752f4e2c36ef18e1df95c2d005a39
Rob Currie <rcurrie@ucsc.edu>
2016-10-24 14:48:58 -0700

    Add download button to search results for integration into xena

---
+++ www/index.js
@@ -0,0 +1,50 @@
+$('document').ready(function() {
+  var resultsTemplate = _.template($("#results-template").html());
+
+  $("#query").focus();
+
+  $("#search").submit(function(event) {
+    query = $("#query").val(); 
+    var request = {
+      "query": {
+        "match": {
+          "_all": {
+            "query": query,
+            "operator": "and"
+          }
+        }
+      },
+      "size": 100 
+    }
+    $.ajax({ 
+      url: "/es/cgt/submission/_search", 
+      type: "POST", 
+      dataType: "json", 
+      data: JSON.stringify(request), 
+      success: function(results) {
+        $("svg").hide();
+        $("#results").html(resultsTemplate({results: results}));
+        var data = "text/json;charset=utf-8,"
+          + encodeURIComponent(JSON.stringify({
+            query: query,
+            submissions: _.pluck(results.hits.hits, "_id")}));
+        $("#download").attr("href", "data:" + data);
+      }
+    }); 
+    event.preventDefault();
+    ga('send', 'event', 'submissions', 'search', query);
+  });
+
+  $.ajax({ 
+    url: "/es/cgt/steward/_search/?size=1000", 
+    type: "GET", 
+    error: function(error) { alert("Search index error, likely undergoing maintenance."); },
+    success: function(results) {
+      if (results.hits.total == 0) {
+        alert("Search index empty, likely undergoing maintenance.");
+      } else {
+        draw_network(_.indexBy(results.hits.hits, '_id'));
+      }
+    }
+  });
+});
